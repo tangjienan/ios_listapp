2c7729f4d767091d5322c31595e25df12b56a201
donezio <donezio32@gmail.com>
2017-11-26 13:11:40 -0800

    first commit

---
+++ list/listObject.swift
@@ -0,0 +1,13 @@
+//
+//  listObject.swift
+//  list
+//
+//  Created by donezio on 11/15/17.
+//  Copyright © 2017 macbook pro. All rights reserved.
+//
+
+import UIKit
+
+class listObject: NSObject {
+
+}
---
+++ list/newCell.swift
@@ -0,0 +1,24 @@
+//
+//  newCell.swift
+//  list
+//
+//  Created by donezio on 11/13/17.
+//  Copyright © 2017 macbook pro. All rights reserved.
+//
+
+import UIKit
+
+class newCell: FoldingCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
